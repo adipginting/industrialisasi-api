f77e7c612d5ddcaa07cbda278f21f49365de56b2
adipginting <adi@adipginting.com>
2022-10-20 11:27:13 +0700

    Cleaning up code

--- src/app.js
+++ src/app.js
@@ -6,8 +6,8 @@ require('dotenv').config();
 
 app.use(express.urlencoded({"extended":"true"}));
 app.use(express.json());
-//app.use(cors({"origin":"http://localhost:3000"}));
-app.use(cors());
+app.use(cors({"origin":"http://localhost:3000"}));
+//app.use(cors());
 app.use('/register', routes.register);
 app.use('/email', routes.email);
 app.use('/username', routes.username);
