898a8350b2ff57a23f64449b2db3822bbdd6fe42
adipginting <adi@adipginting.com>
2023-03-02 20:09:30 +0700

    refactor authorization.js

--- src/routes/authorization.js
+++ src/routes/authorization.js
@@ -4,81 +4,83 @@ const models = require("../models");
 require("dotenv").config();
 
 router.use('/', (req, res, next) => {
-  let access_token = ""; //these blocks test if access code exists or is valid
-  let is_access_token_valid = false;
-  if (req.headers.authorization) {
-    access_token = req.headers.authorization.split(" ")[1];
-    jwt.verify(access_token, process.env.secret_key, (err, result) => {
-      if (err) {
-        console.error(err);
-      } else {
-        res.locals.username = result.username;
-        next();
-      }
-    });
-  }
-
-  let refresh_token = ""; //these blocks test if refresh code exist or is valid
-  let is_refresh_token_valid = false;
-  if (req.cookies) {
-    const cookie_arr = Object.keys(req.cookies);
-    for (let i = 0; i < cookie_arr.length; i++) {
-      if (cookie_arr[i].includes("Bearer")) {
-        refresh_token = cookie_arr[i];
-      }
-    }
-  }
-
-  if (!!refresh_token === true) {
-    is_refresh_token_valid = true;
-  }
-
-  models.authorization(refresh_token).then((does_token_exist) => {
-    if (does_token_exist === false) {
-      is_refresh_token_valid = true;
-    }
-  });
-
-  if (is_refresh_token_valid === true) {
-    jwt.verify(refresh_token, process.env.secret_key, (err, result) => {
-      if (err) {
-        is_refresh_token_valid = false;
-      } else {
-        res.locals.username = result.username; //this is to be used later in the next condition to generate a new access token
-      }
-    });
-  }
-
-  if (is_access_token_valid === false && is_refresh_token_valid === true) {
-    //generate new access and refresh token. Saving old refresh token to db
-    jwt.sign(
-      { username: res.locals.username },
-      process.env.secret_key,
-      { expiresIn: "30m" },
-      (err, token) => {
-        if (err) {
+  const username_in_access_token = () => {
+    let username = "";
+    if (typeof req.headers.authorization !== "undefined") {
+      const access_token = req.headers.authorization.split(" ")[1];
+      try{
+        username = jwt.verify(access_token, process.env.secret_key);
+      } catch (err) {
+        if (err){
           console.error(err);
-        } else {
-          res.locals.access_token = "Bearer " + token;
+          username = "";
         }
       }
-    );
+    }
+    return username;
+  };
 
-    jwt.sign(
-      { username: res.locals.username },
-      process.env.secret_key,
-      { expiresIn: "7d" },
-      (err, token) => {
-        if (err) {
-          console.error(err);
-        } else {
-          res.locals.refresh_token = "Bearer " + token;
-        }
-      }
-    );
-    models.refresh_token(refresh_token);
+  if (username_in_access_token() !== ""){
+    res.locals.get_username();
     next();
   }
+
+  const get_refresh_token = () => {
+    if (typeof req.cookies !== "undefined") {
+      const cookie_arr = Object.keys(req.cookies);
+      for (let i = 0; i < cookie_arr.length; i++) {
+        if (cookie_arr[i].includes("Bearer")) {
+          return cookie_arr[i];
+        }
+      }
+    }
+    return "";
+  };
+
+  const is_refresh_token_on_db = async () => {
+    let does_it_exist = false;
+    try{
+      if (get_refresh_token() !== "") {
+        does_it_exist = await models.authorization(get_refresh_token());
+      }
+    } catch(err){
+      if (err){
+        console.error(err);
+        does_it_exist = false;
+      }
+    }
+    return does_it_exist;
+  };
+
+  const username_in_refresh_token = async () => {
+    let username = "";
+    const refresh_token = get_refresh_token().split(' ')[1];
+    if (await is_refresh_token_on_db() === false) {
+      try{
+        username = jwt.verify(refresh_token, process.env.secret_key);
+      } catch(err){
+        if (err){
+          console.error(err);
+          username = "";
+        }
+      }
+    }
+    return username;
+  };
+
+  const generate_jwt = async () => {
+      try{
+        if (await username_in_access_token() === "" && await username_in_refresh_token() !== "") {
+          res.locals.access_token = "Bearer " + jwt.sign({ username: res.locals.username }, process.env.secret_key, { expiresIn: "30m" });
+          res.locals.refresh_token = "Bearer " + jwt.sign({ username: res.locals.username }, process.env.secret_key, {expiresIn: "7d" });
+          models.refresh_token(get_refresh_token());
+        }
+      } catch (err) {
+        console.error(err);
+      }
+  };
+
+  generate_jwt();
   next();
 });
 
