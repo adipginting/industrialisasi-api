dd108a8d50c274ecff11342f350baa74eb3cd739
adipginting <apg.foss@gmail.com>
2023-11-03 14:40:39 +0700

    fix: cors for www prefixed domain of industrialisasi

--- src/app.js
+++ src/app.js
@@ -7,7 +7,12 @@ require("dotenv").config();
 
 app.use(express.urlencoded({ extended: "true" }));
 app.use(express.json());
-app.use(cors({ credentials: true, origin: "https://industrialisasi.com" }));
+app.use(
+  cors({
+    credentials: true,
+    origin: ["https://industrialisasi.com", "https://www.industrialisasi.com"],
+  }),
+);
 
 app.use(cookieParser());
 app.use("/welcome", routes.welcome);
