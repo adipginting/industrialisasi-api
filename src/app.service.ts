8a0fdfa58d142bdca8cb5b12499ad49d98764bbf
Adi Primanda Ginting <adi@adipginting.com>
2025-03-19 15:38:21 +0700

    feat: pg connection

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Inject } from '@nestjs/common';
+import { PG_CONNECTION } from './constants';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(@Inject(PG_CONNECTION) private connection: any) {}
+
+  async getHello() {
+    const res = await this.connection.query('Select * from users');
+    return res;
   }
 }
